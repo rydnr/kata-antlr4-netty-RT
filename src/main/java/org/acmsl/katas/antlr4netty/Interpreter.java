18bbf0caa046c498e0527b97a1a9f6b1a6307f76
chous <chous@acm-sl.org>
2014-07-15 09:21:36 +0200

    201407150921

--- src/main/java/org/acmsl/katas/antlr4netty/Interpreter.java
+++ src/main/java/org/acmsl/katas/antlr4netty/Interpreter.java
@@ -68,7 +68,7 @@ public class Interpreter
      * Creates a new instance to evaluate given operation.
      * @param op the operation to evaluate.
      */
-    public Interpreter(@NotNull final String op)
+    public Interpreter()
     {
         this.operation = op;
     }
