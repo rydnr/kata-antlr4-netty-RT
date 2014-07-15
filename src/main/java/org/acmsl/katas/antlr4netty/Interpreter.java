6fb4bd63a02af0817c63e0108706b3dd5921d299
chous <chous@acm-sl.org>
2014-07-15 09:33:22 +0200

    201407150933

--- src/main/java/org/acmsl/katas/antlr4netty/Interpreter.java
+++ src/main/java/org/acmsl/katas/antlr4netty/Interpreter.java
@@ -106,6 +106,8 @@ public class Interpreter
                     @NotNull final String left = ctx.getChild(0).getText();
                     @NotNull final String operator = ctx.getChild(1).getText();
                     @NotNull final String right = ctx.getChild(2).getText();
+
+                    if (operator.equals())
                     System.out.println("Intercepted [" + left + ", " + operator + ", " + right + "]");
 
                     return new BigDecimal("3");
