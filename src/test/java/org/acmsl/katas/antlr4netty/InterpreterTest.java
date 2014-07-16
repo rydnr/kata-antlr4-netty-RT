bf972db24a3b77769d68bcfc31bf1cf499751d60
chous <chous@acm-sl.org>
2014-07-16 08:02:42 +0200

    201407160802

--- src/test/java/org/acmsl/katas/antlr4netty/InterpreterTest.java
+++ src/test/java/org/acmsl/katas/antlr4netty/InterpreterTest.java
@@ -75,7 +75,7 @@ public class InterpreterTest
      * Checks whether the interpreter substracts positive, single-digit integers, correctly.
      */
     @Test
-    public void interpreter_sums_negative_single_digit_integers()
+    public void interpreter_substracts_positive_single_digit_integers()
     {
         @NotNull final Interpreter interpreter = new Interpreter();
 
