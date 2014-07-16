cee666caa019d6a509b02ecad45180dbee1e0f82
chous <chous@acm-sl.org>
2014-07-16 08:01:54 +0200

    201407160801

--- src/test/java/org/acmsl/katas/antlr4netty/InterpreterTest.java
+++ src/test/java/org/acmsl/katas/antlr4netty/InterpreterTest.java
@@ -70,4 +70,15 @@ public class InterpreterTest
         Assert.assertEquals(new BigDecimal("3"), interpreter.eval("1+2"));
         Assert.assertEquals(new BigDecimal("10"), interpreter.eval("1+9"));
     }
+    /**
+     * Checks whether the interpreter sums positive, single-digit integers, correctly.
+     */
+    @Test
+    public void interpreter_sums_positive_single_digit_integers()
+    {
+        @NotNull final Interpreter interpreter = new Interpreter();
+
+        Assert.assertEquals(new BigDecimal("3"), interpreter.eval("1+2"));
+        Assert.assertEquals(new BigDecimal("10"), interpreter.eval("1+9"));
+    }
 }
