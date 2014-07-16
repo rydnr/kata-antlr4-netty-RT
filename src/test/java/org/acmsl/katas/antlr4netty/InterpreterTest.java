65beb9c0ce9bcc06b48997b25b8b63a866c43339
chous <chous@acm-sl.org>
2014-07-16 08:02:56 +0200

    201407160802

--- src/test/java/org/acmsl/katas/antlr4netty/InterpreterTest.java
+++ src/test/java/org/acmsl/katas/antlr4netty/InterpreterTest.java
@@ -80,6 +80,6 @@ public class InterpreterTest
         @NotNull final Interpreter interpreter = new Interpreter();
 
         Assert.assertEquals(new BigDecimal("3"), interpreter.eval("4-1"));
-        Assert.assertEquals(new BigDecimal("10"), interpreter.eval("5-9"));
+        Assert.assertEquals(new BigDecimal("10"), interpreter.eval("5-2"));
     }
 }
