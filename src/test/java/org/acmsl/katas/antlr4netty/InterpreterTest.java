7f91f1f451801055de8e9c643f4038b9943423bc
chous <chous@acm-sl.org>
2014-07-16 08:02:08 +0200

    201407160802

--- src/test/java/org/acmsl/katas/antlr4netty/InterpreterTest.java
+++ src/test/java/org/acmsl/katas/antlr4netty/InterpreterTest.java
@@ -75,7 +75,7 @@ public class InterpreterTest
      * Checks whether the interpreter sums negative, single-digit integers, correctly.
      */
     @Test
-    public void interpreter_sums_positive_single_digit_integers()
+    public void interpreter_sums_negaitive_single_digit_integers()
     {
         @NotNull final Interpreter interpreter = new Interpreter();
 
