b3cfa1c305a6d1ffc5234ec4c8206f503c2243a8
chous <chous@acm-sl.org>
2014-07-16 08:04:25 +0200

    201407160804

--- src/test/java/org/acmsl/katas/antlr4netty/InterpreterTest.java
+++ src/test/java/org/acmsl/katas/antlr4netty/InterpreterTest.java
@@ -87,7 +87,7 @@ public class InterpreterTest
      * Checks whether the interpreter sums positive, double-digit integers, correctly.
      */
     @Test
-    public void interpreter_sums_positive_single_digit_integers()
+    public void interpreter_sums_positive_double_digit_integers()
     {
         @NotNull final Interpreter interpreter = new Interpreter();
 
